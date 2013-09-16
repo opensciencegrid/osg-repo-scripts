452694331574097ed2542e87a14de05a19f2f9da
Carl Edquist <edquist@cs.wisc.edu>
2013-09-16 21:30:06 +0000

    have update_mirror script use osg-tags and new repo layout
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-next-tools/mash-scripts/osg-next@17709 4e558342-562e-0410-864c-e07659590f8c

--- usr/local/update_mirror.py
+++ usr/local/update_mirror.py
@@ -7,21 +7,25 @@ import commands
 from socket import gethostname
 
 def log(log):
-        print time.strftime("%a %m/%d/%y %H:%M:%S %Z: ", time.localtime()),log
+    print time.strftime("%a %m/%d/%y %H:%M:%S %Z: ", time.localtime()),log
 
-repos = ["osg-development", "osg-release", "osg-testing", "osg-contrib", "osg-upcoming-development", "osg-upcoming-release", "osg-upcoming-testing"]
+tagfile = open("/usr/local/osg-tags", "r")
+tags = [tag.rstrip("\n") for tag in tagfile]
+tagfile.close()
+
 archs = ["i386", "x86_64"]
-oshosts = {
-    "el5": ["http://mirror.hep.wisc.edu/upstream", "http://mirror.batlab.org/repos/osg/3.0/el5", "http://t2.unl.edu/osg/3.0/el5"],
-    "el6": ["http://mirror.batlab.org/repos/osg/3.0/el6", "http://t2.unl.edu/osg/3.0/el6"]
-}
-version = "3.0"
+mirrorhosts = [
+    # list of mirror base urls, where osg/series/dver/repo/arch can be found
+    "http://mirror.hep.wisc.edu/upstream",
+    "http://mirror.batlab.org/repos",
+    "http://t2.unl.edu"
+]
 
 threshold = 24 #hours
 
 log("Using following parameters")
-log("repos:"+str(repos))
-log("hosts:"+str(oshosts))
+log("tags:"+str(tags))
+log("hosts:"+str(mirrorhosts))
 log("archs:"+str(archs))
 log("threshold:"+str(threshold)+" (hours)")
 print
@@ -29,62 +33,67 @@ print
 #gethostname() returns actual instance name (like repo2.grid.iu.edu)
 hostname="repo.grid.iu.edu"
 if gethostname() == "repo-itb.grid.iu.edu":
-	hostname="repo-itb.grid.iu.edu"
+    hostname="repo-itb.grid.iu.edu"
 
-def test(os,hosts,repo,arch):
-        list = ["http://"+hostname+"/"+version+"/"+os+"/"+repo+"/"+arch] #repo.grid should always exist
-        for host in hosts:
-                url=host+"/"+repo+"/"+arch
-                mdurl=url+"/repodata/repomd.xml"
-                log("checking: "+mdurl)
-                try:
-                        response = urllib2.urlopen(mdurl)
-                        if response.code != 200:
-                                log("\tbad(non 200) response.code:"+response.code)
-                        else:
-                                #make sure the repository is up-to-date
-                                lastmod_str = response.headers["Last-Modified"]
-                                lastmodtime = time.strptime(lastmod_str, "%a, %d %b %Y %H:%M:%S %Z") #Thu, 15 Sep 2011 13:34:06 GMT
-                                age = (time.mktime(time.gmtime()) - time.mktime(lastmodtime))
-                                if age > 3600 * threshold:
-                                        log("\ttoo old ("+str(age)+" seconds old) Last-Modified: "+lastmod_str+" .. ignoring")
-                                else:
-                                        list.append(url)
-                                        log("\tall good")
-                except urllib2.HTTPError,e:
-                        #no such repo on this host..
-                        log("\tURL caught while processing url:"+url+" "+str(e))
-		except Exception, e:
-                        log("\tException caught while processing url:"+url+" "+str(e))
-				
-        return list
+def mkarchurl(host,tag,arch):
+    osg,series,dver,repo = tag.split('-')
+    return '/'.join([host,osg,series,dver,repo,arch])
 
-for os in oshosts.keys():
-    log("evacuating live dir for "+os)
+def test(hosts,tag,arch):
+    # repo.grid should always exist
+    list = [mkarchurl('http://'+hostname,tag,arch)]
+    for host in hosts:
+        url = mkarchurl(host,tag,arch)
+        mdurl=url+"/repodata/repomd.xml"
+        log("checking: "+mdurl)
+        try:
+            response = urllib2.urlopen(mdurl)
+            if response.code != 200:
+                log("\tbad(non 200) response.code:"+response.code)
+            else:
+                #make sure the repository is up-to-date
+                lastmod_str = response.headers["Last-Modified"]
+                lastmodtime = time.strptime(lastmod_str, "%a, %d %b %Y %H:%M:%S %Z") #Thu, 15 Sep 2011 13:34:06 GMT
+                age = (time.mktime(time.gmtime()) - time.mktime(lastmodtime))
+                if age > 3600 * threshold:
+                    log("\ttoo old ("+str(age)+" seconds old) Last-Modified: "+lastmod_str+" .. ignoring")
+                else:
+                    list.append(url)
+                    log("\tall good")
+        except urllib2.HTTPError,e:
+            #no such repo on this host..
+            log("\tURL caught while processing url:"+url+" "+str(e))
+        except Exception, e:
+            log("\tException caught while processing url:"+url+" "+str(e))
+
+    return list
+
+for tag in tags:
+    log("evacuating live dir for osg")
 
     #creating new mirror_prev
-    commands.getstatusoutput("rm -rf /usr/local/mirror/"+version+"/."+os+".prev")
-    commands.getstatusoutput("cp -r /usr/local/mirror/"+version+"/."+os+".new /usr/local/mirror/"+version+"/."+os+".prev")
+    commands.getstatusoutput("rm -rf /usr/local/mirror/.osg.prev")
+    commands.getstatusoutput("cp -a /usr/local/mirror/.osg.new /usr/local/mirror/.osg.prev")
 
     #pointing mirror to previous
-    commands.getstatusoutput("ln -f -s -T /usr/local/mirror/"+version+"/."+os+".prev /usr/local/mirror/"+version+"/"+os)
+    commands.getstatusoutput("ln -f -s -T .osg.prev /usr/local/mirror/osg")
 
     #empty new mirror
-    commands.getstatusoutput("rm -rf /usr/local/mirror/"+version+"/."+os+".new/*")
+    commands.getstatusoutput("rm -rf /usr/local/mirror/.osg.new/*")
 
-    log("checking for "+os)
-    hosts = oshosts[os]
-    for repo in repos:
-            for arch in archs:
-                    list = test(os,hosts,repo,arch)
-                    commands.getstatusoutput("mkdir -p /usr/local/mirror/"+version+"/."+os+".new/"+repo)
-                    f = open("/usr/local/mirror/"+version+"/."+os+".new/"+repo+"/"+arch, "w")
-                    for m in list:
-                            f.write(m+"\n")
-                    f.close()       
+    log("checking for "+tag)
+    osg,series,dver,repo = tag.split('-')
+    for arch in archs:
+        list = test(mirrorhosts,tag,arch)
+        repopath = '/'.join(["/usr/local/mirror/.osg.new",series,dver,repo])
+        commands.getstatusoutput("mkdir -p " + repopath)
+        f = open(repopath + "/" + arch, "w")
+        for m in list:
+            f.write(m+"\n")
+        f.close()       
 
     #pointing mirror to mirror_new
-    commands.getstatusoutput("ln -s -f -T /usr/local/mirror/"+version+"/."+os+".new /usr/local/mirror/"+version+"/"+os)
+    commands.getstatusoutput("ln -f -s -T .osg.new /usr/local/mirror/osg")
 
 log("all done")
 
