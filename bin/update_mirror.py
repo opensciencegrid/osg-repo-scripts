f61adc91c519146c75fa228aee2d4c3fff67ac92
efajardo <efajardo@physics.ucsd.edu>
2018-05-29 13:43:58 -0700

    Remove CHTC from the mirrorlist

--- bin/update_mirror.py
+++ bin/update_mirror.py
@@ -34,7 +34,6 @@ tagfile.close()
 archs = ["i386", "x86_64"]
 mirrorhosts = [
     # list of mirror base urls, where osg/series/dver/repo/arch can be found
-    "http://mirror.hep.wisc.edu/upstream",
     "http://mirror.batlab.org/repos",
     "http://t2.unl.edu",
     "http://mirror.grid.uchicago.edu/pub"
