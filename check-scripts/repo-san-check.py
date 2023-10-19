e0a5d48acdcfada47532385ee2619000bea1793e
Matyas Selmeci <matyas@cs.wisc.edu>
2023-10-19 14:39:33 -0500

    Cleanup

--- check-scripts/repo-san-check.py
+++ check-scripts/repo-san-check.py
@@ -2,24 +2,22 @@
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
 from html.parser import HTMLParser
-from pathlib import Path
 from subprocess import run
 from typing import NamedTuple
 import functools
-import glob
-import os
-import pathlib
-import re
 import requests
 import shutil
 import subprocess
 import sys
 
-if __name__ == "__main__" and __package__ is None:
-    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 REPO = "repo.opensciencegrid.org"
 REPO_RSYNC = "repo-rsync.opensciencegrid.org"
+
+
+HTTP = "http"
+RSYNC = "rsync"
+METHODS = [HTTP, RSYNC]
 
 
 class TagAndDirectory(NamedTuple):
@@ -28,13 +26,19 @@ class TagAndDirectory(NamedTuple):
     should_have_condor: bool
 
 
-class HTMLDirListParser(HTMLParser):
+class DirListParser:
     def __init__(self):
-        super().__init__()
         self.dir_listing = []
         self.rpm_listing = []
-        # self.in_link = False
-        # self.current_href = None
+
+    def read_data(self, data: str) -> None:
+        raise NotImplementedError()
+
+
+class HTMLDirListParser(HTMLParser, DirListParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        DirListParser.__init__(self)
 
     def handle_starttag(self, tag, attrs):
         #print(tag, attrs)
@@ -46,32 +50,12 @@ class HTMLDirListParser(HTMLParser):
             elif not href.startswith((".", "/")) and href.endswith("/"):
                 self.dir_listing.append(href[:-1])
 
-            # self.in_link = True
-            # self.current_href = attrs_d["href"]
-
-    #def handle_data(self, data):
-    #    if self.in_link:
-    #        #print(data)
-    #        if 
-    #        if data == self.current_href:
-    #            # This looks like a file or directory link
-    #            if data.endswith("/"):
-    #                self.dir_listing.append(data)
-    #            else:
-    #                self.files_listing.append(data)
-
-    #def handle_endtag(self, tag):
-    #    if tag == "a":
-    #        self.in_link = False
-    #        self.current_href = None
+    def read_data(self, data: str) -> None:
+        self.feed(data)
 
 
-class RsyncDirListParser:
-    def __init__(self):
-        self.dir_listing = []
-        self.rpm_listing = []
-
-    def handle_line(self, line):
+class RsyncDirListParser(DirListParser):
+    def handle_line(self, line: str) -> None:
         try:
             mode, size, date, time, name = line.split(None, 4)
         except ValueError:
@@ -81,8 +65,8 @@ class RsyncDirListParser:
         elif name.endswith(".rpm"):
             self.rpm_listing.append(name)
 
-    def feed(self, text):
-        for line in text.splitlines():
+    def read_data(self, data: str) -> None:
+        for line in data.splitlines():
             self.handle_line(line)
 
 
@@ -102,12 +86,18 @@ def main(argv=None):
         argv = sys.argv
 
     parser = ArgumentParser()
-    parser.add_argument("method", choices=["http", "rsync"], help="The method to check with (http or rsync)")
-    parser.add_argument("repo", help="The repo host to check", nargs="?", default=None)
-    parser.add_argument("--no-koji", help="Do not compare repo against tags in koji", dest="koji", action="store_false")
+    parser.add_argument("method",
+                        help="The method to check with (http or rsync)",
+                        choices=METHODS)
+    parser.add_argument("repo",
+                        help="The repo host to check",
+                        nargs="?", default=None)
+    parser.add_argument("--no-koji",
+                        help="Do not compare repo against tags in koji",
+                        dest="koji", action="store_false")
     args = parser.parse_args(argv[1:])
     method = args.method
-    repo = args.repo if args.repo else (REPO if method == "http" else REPO_RSYNC)
+    repo = args.repo if args.repo else (REPO if method == HTTP else REPO_RSYNC)
     koji = args.koji
 
     if koji and not shutil.which("osg-koji"):
@@ -131,14 +121,14 @@ def main(argv=None):
     ]
     # TODO: Add contrib, empty, 3.5, etc.
 
-    assert method in ["http", "rsync"], f"bad method {method} should have been caught"
+    assert method in METHODS, f"bad method {method} should have been caught"
 
-    if method == "http":
+    if method == HTTP:
         try:
             resp = requests.get(f"https://{repo}/osg")
-        except requests.exception.ConnectionError as err:
+        except requests.ConnectionError as err:
             return f"Can't connect to repo {repo} via HTTP: {err}"
-    elif method == "rsync":
+    elif method == RSYNC:
         ret = run(["rsync", f"rsync://{repo}/osg/"], stdout=subprocess.PIPE)
         if ret.returncode != 0:
             return f"Can't get listing from repo {repo} via RSYNC"
@@ -153,17 +143,19 @@ def main(argv=None):
         if koji:
             expected_num_srpms = len(get_koji_listing(tag))
 
-        if method == "http":
+        dlp = None
+        if method == HTTP:
             resp = requests.get(f"https://{repo}/{dir_}")
             dlp = HTMLDirListParser()
-            dlp.feed(resp.text)
-        elif method == "rsync":
+            dlp.read_data(resp.text)
+        elif method == RSYNC:
             ret = run(["rsync", f"rsync://{repo}/{dir_}/"], stdout=subprocess.PIPE, encoding="latin-1")
             if ret.returncode != 0:
                 print(f"{dir_} could not be queried")
                 continue
             dlp = RsyncDirListParser()
-            dlp.feed(ret.stdout)
+            dlp.read_data(ret.stdout)
+        assert isinstance(dlp, DirListParser)
 
         if "repodata" not in dlp.dir_listing:
             print(f"{dir_} does not have a repodata dir")
