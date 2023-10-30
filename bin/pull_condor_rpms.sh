472b893fca0456ef5a5fdd5024a770a62c041a54
Matthew Westphall <westphall@wisc.edu>
2023-10-30 11:41:43 -0500

    log more detailed information about rsync failures

--- bin/pull_condor_rpms.sh
+++ bin/pull_condor_rpms.sh
@@ -74,8 +74,22 @@ mkdir -p $CURRENT_REPO_DIR
 for CONDOR_REPO in ${CONDOR_REPOS[@]}; do
   RSYNC_URL="$RSYNC_ROOT/$CONDOR_SERIES/$DVER/x86_64/$CONDOR_REPO/$SOURCE_SET*.rpm"
   echo "rsyncing $RSYNC_URL to $NEW_REPO_DIR"
-  if ! rsync --times $RSYNC_URL $NEW_REPO_DIR --link-dest $CURRENT_REPO_DIR; then
-    echo "Error: No packages found for $RSYNC_URL."
+  rsync_tmpfile=$(mktemp rsync.XXXXXX)
+
+  rsync --list-only "$RSYNC_URL" > "$rsync_tmpfile"
+  ret=$?
+  if [[ $ret != 0 ]]; then
+    echo "Unable to get directory listing for $RSYNC_URL: rsync failed with exit code $ret"
+    exit 1
+  elif [[ $(wc -l "$rsync_tmpfile") == 0 ]]; then
+    echo "Directory listing for $RSYNC_URL returned no files"
+    exit 1
+  fi
+
+  rsync --times $RSYNC_URL $NEW_REPO_DIR --link-dest $CURRENT_REPO_DIR
+  ret=$?
+  if [[ $ret != 0 ]]; then
+    echo "Unable to retrieve htcondor packages for $RSYNC_URL: rsync failed with exit code $ret"
     exit 1
   fi
 done
