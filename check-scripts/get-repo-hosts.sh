e737b02ab6bd5ba211a7d04f5bc53e549b8a0639
Matyas Selmeci <matyas@cs.wisc.edu>
2024-07-22 13:31:33 -0500

    get-repo-hosts.sh: check repo-rsync-itb; widen columns

--- check-scripts/get-repo-hosts.sh
+++ check-scripts/get-repo-hosts.sh
@@ -3,9 +3,9 @@
 
 command -v nslookup &>/dev/null || { echo >&2 "nslookup not found"; exit 127; }
 
-for host in {repo,repo-rsync,repo-itb}.{opensciencegrid.org,osg-htc.org}; do
+for host in {repo,repo-rsync,repo-itb,repo-rsync-itb}.{opensciencegrid.org,osg-htc.org}; do
     realname=$(nslookup ${host} | awk '/^Name:/ { print $2; exit }')
     realname=${realname:-NOTFOUND}
-    printf "%31s %31s\n" "$host" "$realname"
+    printf "%39s %39s\n" "$host" "$realname"
 done
 
